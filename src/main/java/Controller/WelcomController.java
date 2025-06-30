804c046f88e48f8d6bac06994ef1edf89a0afb76
SagarPradeepTumkur <Sagarpradeep456@gmail.com>
2025-06-30 17:33:59 +0000

    Updated files

--- src/main/java/Controller/WelcomController.java
+++ src/main/java/Controller/WelcomController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController {
+public class WelcomController {
     @GetMapping("/welcome")
     public String getWelcomeMessage() {
         return "Welcome to the Spring Boot Web Application!";
