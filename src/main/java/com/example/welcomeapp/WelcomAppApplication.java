136de1ab03e22805b1e4b4162484612c76f3e7bf
SagarPradeepTumkur <Sagarpradeep456@gmail.com>
2025-06-30 18:06:56 +0000

    Updated file

--- src/main/java/com/example/welcomeapp/WelcomAppApplication.java
+++ src/main/java/com/example/welcomeapp/WelcomAppApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WelcomAppApplication {
     public static void main(String[] args) {
-        SpringApplication.run(WelcomeAppApplication.class, args);
+        SpringApplication.run(WelcomAppApplication.class, args);
     }
 }
